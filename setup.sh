ca7e1a6ececf0ec27fba4a52a14e262167d55a74
Apoorv Reddy <areddy@cognitivescale.com>
2016-05-05 12:34:51 +0530

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget "http://downloads.sourceforge.net/project/ctakesresources/ctakes-resources-3.2.1.1-bin.zip?r=&ts=1461058782&use_mirror=iweb"
+unzip ctakes-resources-3.2.1.1-bin.zip
+mv ctakes-resources-3.2.1.1-bin/resources resources
+rm ctakes-resources-3.2.1.1-bin.zip
+echo "Downloaded  ctakes-resources"
+
+mvn package
+echo "package built"
+echo "Run Server using the following command: java -Dctakes.umlsuser=<username> -Dctakes.umlspw=<passwd> -jar target/claims-nlp.0.0.1-SNAPSHOT.jar server"
